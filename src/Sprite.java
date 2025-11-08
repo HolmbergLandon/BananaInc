9dc438a00081f9940521d6e4a6d9af85f13f0918
Ripley Becker <ripleybecker2006@gmail.com>
2025-11-08 17:23:13 -0600

    feat: update image loading logic for cross-platform compatibility and remove redundant code

--- src/Sprite.java
+++ src/Sprite.java
@@ -6,23 +6,43 @@ import javax.swing.*;
 public class Sprite {
     private static Map<String, Image> images = new HashMap<>();
     
+    
     static {
+        if(System.getProperty("os.name").toLowerCase().contains("win")) {
+            loadImage("bananaTier1", "./BananaInc/sprites/oneBanana.png");
+            loadImage("bananaTier2", "./BananaInc/sprites/twoBanana.png");
+            loadImage("bananaTier3", "./BananaInc/sprites/threeBanana.png");
+            loadImage("bananaTier4", "./BananaInc/sprites/fourBanana.png");
+            loadImage("backgroundTier1", "./BananaInc/sprites/tier1Background.png");
+            loadImage("backgroundTier2", "./BananaInc/sprites/tier2Background.png");
+            loadImage("backgroundTier3", "./BananaInc/sprites/tier3Background.png");
+            loadImage("backgroundTier4", "./BananaInc/sprites/tier4Background.png");
+            loadImage("Cursor", "./BananaInc/sprites/cursor.png");
+            loadImage("Laborer", "./BananaInc/sprites/laborer.png");
+            loadImage("Farm", "./BananaInc/sprites/farm.png");
+            loadImage("Crawlerr", "./BananaInc/sprites/crawler.png");
+            loadImage("Plantation", "./BananaInc/sprites/plantation.png");
+            loadImage("GMO Factory", "./BananaInc/sprites/GMOFactory.png");
+            loadImage("Alternate Universe", "./BananaInc/sprites/alternateUniverse.png");
+        } else {
+            loadImage("bananaTier1", "sprites/oneBanana.png");
+            loadImage("bananaTier2", "sprites/twoBanana.png");
+            loadImage("bananaTier3", "sprites/threeBanana.png");
+            loadImage("bananaTier4", "sprites/fourBanana.png");
+            loadImage("backgroundTier1", "sprites/tier1Background.png");
+            loadImage("backgroundTier2", "sprites/tier2Background.png");
+            loadImage("backgroundTier3", "sprites/tier3Background.png");
+            loadImage("backgroundTier4", "sprites/tier4Background.png");
+            loadImage("Cursor", "sprites/cursor.png");
+            loadImage("Laborer", "sprites/laborer.png");
+            loadImage("Farm", "sprites/farm.png");
+            loadImage("Crawlerr", "sprites/crawler.png");
+            loadImage("Plantation", "sprites/plantation.png");
+            loadImage("GMO Factory", "sprites/GMOFactory.png");
+            loadImage("Alternate Universe", "sprites/alternateUniverse.png");
+        }
         // Load tier-based images
-        loadImage("bananaTier1", "sprites/oneBanana.png");
-        loadImage("bananaTier2", "sprites/twoBanana.png");
-        loadImage("bananaTier3", "sprites/threeBanana.png");
-        loadImage("bananaTier4", "sprites/fourBanana.png");
-        loadImage("backgroundTier1", "sprites/tier1Background.png");
-        loadImage("backgroundTier2", "sprites/tier2Background.png");
-        loadImage("backgroundTier3", "sprites/tier3Background.png");
-        loadImage("backgroundTier4", "sprites/tier4Background.png");
-        loadImage("Cursor", "sprites/cursor.png");
-        loadImage("Laborer", "sprites/laborer.png");
-        loadImage("Farm", "sprites/farm.png");
-        loadImage("Crawlerr", "sprites/crawler.png");
-        loadImage("Plantation", "sprites/plantation.png");
-        loadImage("GMO Factory", "sprites/GMOFactory.png");
-        loadImage("Alternate Universe", "sprites/alternateUniverse.png");
+        
     }
     
     /**
