c5fbc30e479cef14183ce93529dafc496df1f5bd
Ripley Becker <ripleybecker2006@gmail.com>
2025-11-08 18:44:14 -0600

    feat: initialize player with a large number of bananas

--- src/Game.java
+++ src/Game.java
@@ -3,6 +3,7 @@ import javax.swing.SwingUtilities;
 public class Game {
     public static void main(String[] args) {
         Player player = new Player("Player");
+        player.bananas = 100000000000000.0;
         Building.initBuildingList();
         Upgrade.initUpgrades();
         SwingUtilities.invokeLater(() -> {new BananaClickerGUI(player).setVisible(true);});
